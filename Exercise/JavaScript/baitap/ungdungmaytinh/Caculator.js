3498676940ed4c164d0ca3f5d9164335462a651e
NhatTran95 <tranducanhnhat@gmail.com>
2023-04-11 09:01:59 +0700

    up

---
+++ Exercise/JavaScript/baitap/ungdungmaytinh/Caculator.js
@@ -0,0 +1,24 @@
+function cong(){
+    let X = parseFloat(document.getElementById('nb1').value);
+    let Y = parseFloat(document.getElementById('nb2').value);
+    let sum = X + Y;
+    document.getElementById('result').innerText=''+ sum;
+}
+function tru(){
+    let X = parseFloat(document.getElementById('nb1').value);
+    let Y = parseFloat(document.getElementById('nb2').value);
+    let sub = X - Y;
+    document.getElementById('result').innerText=''+ sub;
+}
+function nhan(){
+    let X = parseFloat(document.getElementById('nb1').value);
+    let Y = parseFloat(document.getElementById('nb2').value);
+    let mul = X * Y;
+    document.getElementById('result').innerText=''+ mul;
+}
+function chia(){
+    let X = parseFloat(document.getElementById('nb1').value);
+    let Y = parseFloat(document.getElementById('nb2').value);
+    let div = X / Y;
+    document.getElementById('result').innerText=''+ div.toFixed(2);
+}
