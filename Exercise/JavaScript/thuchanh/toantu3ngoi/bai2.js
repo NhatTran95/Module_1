18b03ca97c62fd380a81faa797098be3f14d9989
NhatTran95 <tranducanhnhat@gmail.com>
2023-04-12 23:27:17 +0700

    up

--- Exercise/JavaScript/thuchanh/toantu3ngoi/bai2.js
+++ Exercise/JavaScript/thuchanh/toantu3ngoi/bai2.js
@@ -2,3 +2,4 @@ let message = prompt('Enter a message:');
 message=="Employee" ? alert('Hello'):
     message=="Director" ? alert('Greetings'):
         message=="" ? alert('No login'): alert('')
+alert(message);
