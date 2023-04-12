41c3c33d3c30e9325ec3d26408697562fbbb53cd
NhatTran95 <tranducanhnhat@gmail.com>
2023-04-12 09:09:23 +0700

    up

---
+++ Exercise/JavaScript/thuchanh/if_else_if/index.js
@@ -0,0 +1,19 @@
+let yourName = prompt('Enter the name:');
+if ( yourName == 'Admin'){
+    let yourPassword = prompt('Enter password:');
+    if (yourPassword == 'TheMaster'){
+        alert('Welcome');
+    }else{
+        if (yourPassword == 'null'){
+            alert('Canceled')
+        }else{
+            alert('Wrong password')
+        }
+    }
+}else{
+    if (yourName == 'null'){
+        alert('canceld')
+    }else{
+        alert("I don't know you")
+    }
+}
