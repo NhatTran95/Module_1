cedf2ed2b39a3f043702f02b70d9bb09a8a0c09a
NhatTran95 <tranducanhnhat@gmail.com>
2023-04-12 12:01:50 +0700

    up

---
+++ Exercise/JavaScript/thuchanh/toantu3ngoi/bai2.js
@@ -0,0 +1,4 @@
+let message = prompt('Enter a message:');
+message=="Employee" ? alert('Hello'):
+    message=="Director" ? alert('Greetings'):
+        message=="" ? alert('No login'): alert('')
---
+++ Exercise/JavaScript/thuchanh/toantu3ngoi/index.js
@@ -0,0 +1,4 @@
+let a = parseFloat(prompt("a="));
+let b = parseFloat(prompt('b='));
+let sum = a + b;
+sum < 4 ? alert('Below'): alert('Over');
