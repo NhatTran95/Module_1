412fba36f3042522cd6fa470d670637374c49624
NhatTran95 <tranducanhnhat@gmail.com>
2023-04-13 21:16:30 +0700

    up

--- Exercise/JavaScript/baitap/bien-kieudulieu-toantu/chuyendoinhietdo.js
+++ Exercise/JavaScript/baitap/bien-kieudulieu-toantu/chuyendoinhietdo.js
@@ -1,5 +1,5 @@
 function chang(){
     let C = parseFloat(document.getElementById('do_C').value);
     let F = ( C * 9 / 5) + 32;
-    document.getElementById('do_F').innerText='chuyển sang độ F bằng: '+ F + 'oF';
+    document.getElementById('do_F').innerText= C + ' oC bằng: '+ F + ' oF';
 }
--- Exercise/JavaScript/baitap/ungdungmaytinh/Caculator.js
+++ Exercise/JavaScript/baitap/ungdungmaytinh/Caculator.js
@@ -2,23 +2,23 @@ function cong(){
     let X = parseFloat(document.getElementById('nb1').value);
     let Y = parseFloat(document.getElementById('nb2').value);
     let sum = X + Y;
-    document.getElementById('result').innerText=''+ sum;
+    document.getElementById('result').innerText= sum;
 }
 function tru(){
     let X = parseFloat(document.getElementById('nb1').value);
     let Y = parseFloat(document.getElementById('nb2').value);
     let sub = X - Y;
-    document.getElementById('result').innerText=''+ sub;
+    document.getElementById('result').innerText= sub;
 }
 function nhan(){
     let X = parseFloat(document.getElementById('nb1').value);
     let Y = parseFloat(document.getElementById('nb2').value);
     let mul = X * Y;
-    document.getElementById('result').innerText=''+ mul;
+    document.getElementById('result').innerText= mul;
 }
 function chia(){
     let X = parseFloat(document.getElementById('nb1').value);
     let Y = parseFloat(document.getElementById('nb2').value);
     let div = X / Y;
-    document.getElementById('result').innerText=''+ div.toFixed(2);
+    document.getElementById('result').innerText= div.toFixed(2);
 }
